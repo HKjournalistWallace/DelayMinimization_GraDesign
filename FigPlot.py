90b53193f8b99bb26f93b89b2fcf471051f8d28c
FrankXu@Home <frankxu@cqu.edu.cn>
2022-01-20 19:03:26 +0800

    Ver 1.0: -

---
+++ FigPlot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+## Delay Plotting
+K_axis = range(1,10)
+J10S = [320 for _ in K_axis]
+J10P = [313,220,189,173,173,158,142,142,142]
+J10G = [313,237,206,206,206,206,206,206,206]
+J6S = [192 for _ in K_axis]
+J6P = [189,158,142,142,142,142,142,142,142]
+J6G = [189,159,159,159,159,159,159,159,159]
+
+# rcParams['font.family'] = 'Times New Roman'
+rcParams['font.family'] = 'cmr10'
+rcParams['mathtext.default'] = 'regular'
+
+
+
+fig, ax = plt.subplots(1,1,figsize=(8,8),dpi=200)
+ax.set_xlabel("$K$")
+ax.set_ylabel("$t_J$ [ms]")
+ax.set_ylim(125,325)
+# ax.set_xlim(1,9)
+ax.plot(K_axis,J10S,'r--',marker='d',markersize=5,label='Serial DAG with ${J=10}$')
+ax.plot(K_axis,J10P,'y--',marker='d',markersize=5,label='Parallel DAG with $J=10$')
+ax.plot(K_axis,J10G,'c--',marker='d',markersize=5,label='General DAG with $J=10$')
+ax.plot(K_axis,J6S,'r',marker='o',markersize=5,label='Serial DAG with $J=6$')
+ax.plot(K_axis,J6P,'y',marker='o',markersize=5,label='Parallel DAG with $J=6$')
+ax.plot(K_axis,J6G,'c',marker='o',markersize=5,label='General DAG with $J=6$')
+ax.legend(loc=(0.6,0.5))
+ax.grid(ls='-.')
+fig.savefig('./DelayPlot.svg')
