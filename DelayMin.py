8eb1f5775a010c81e46f4ddd45b39ad428f28f55
FrankXu@Home <frankxu@cqu.edu.cn>
2022-01-20 12:46:33 +0800

    Ver 1.1: Added __main__ judge

--- DelayMin.py
+++ DelayMin.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import gurobipy as gp
 from gurobipy import GRB
 import math
@@ -25,8 +26,6 @@ class Params:
     # DAG_structure = 'Serial'                # DAG Structure, 'Serial', 'Parallel' or 'General' 
     time_window   = 1*10**(-3)              # time window 1ms
 
-## Model
-model = gp.Model("OptimalOffloading")
 
 
 ## Variables Initialize
@@ -87,81 +86,92 @@ def Get_Rlk():
 
 E = Get_Edge(Params.DAG_structure)
 
-print('--------------------------Edge Matrix---------------------------')
-for i in range(Params.J):
-    print(f'{E[i]}\n')
-
 Rlk = Get_Rlk()
 
-# Define variables
-x  = model.addVars(Params.J, Params.K+1, Params.slots, vtype = GRB.BINARY)
-a  = model.addVars(Params.J, Params.K+1, vtype = GRB.BINARY)
-tj = model.addVars(Params.J, lb=0, ub=Params.slots, vtype = GRB.INTEGER)
-z  = model.addVars(Params.J, lb=0, vtype = GRB.INTEGER) #T_j
-model.update()
+if __name__ == '__main__' :
+    ## Model
+    model = gp.Model("OptimalOffloading")
 
-# time slots
-t = [ _ for _ in range(Params.slots)]
+    # Define variables
+    x  = model.addVars(Params.J, Params.K+1, Params.slots, vtype = GRB.BINARY)
+    a  = model.addVars(Params.J, Params.K+1, vtype = GRB.BINARY)
+    tj = model.addVars(Params.J, lb=0, ub=Params.slots, vtype = GRB.INTEGER)
+    z  = model.addVars(Params.J, lb=0, vtype = GRB.INTEGER) #T_j
+    model.update()
 
-# a & tj
-model.addConstrs(a[j,k]==gp.quicksum(x[j,k,_] for _ in range(Params.slots)) for j in range(Params.J) for k in range(Params.K+1))
-model.addConstrs(tj[j]==gp.quicksum(t[i]*x[j,k,i] for k in range(Params.K+1) for i in range(Params.slots)) for j in range(Params.J))
+    # time slots
+    t = [ _ for _ in range(Params.slots)]
 
-## Add Constraints
-# Add constraint T_j = max_{i<j}...
-for j1 in range(Params.J):
-    for i1 in range(j1):
-        model.addConstr(z[j1]>=E[i1][j1]*tj[i1]+E[i1][j1]*gp.quicksum([E[i1][j1]*a[j1,k]*a[i1,l]*math.ceil((Params.d_j/Rlk[l][k])/Params.time_window) for l in range(Params.K+1) for k in range(Params.K+1)]))
+    # a & tj
+    model.addConstrs(a[j,k]==gp.quicksum(x[j,k,_] for _ in range(Params.slots)) for j in range(Params.J) for k in range(Params.K+1))
+    model.addConstrs(tj[j]==gp.quicksum(t[i]*x[j,k,i] for k in range(Params.K+1) for i in range(Params.slots)) for j in range(Params.J))
 
-# Compute t_j^c
-t_jc={}
-for i1 in range(Params.J):
-    t_jc[i1] = gp.quicksum(a[i1,k]*math.ceil((Params.c_j/Params.f_k)/Params.time_window) for k in range(Params.K+1))
+    ## Add Constraints
+    # Add constraint T_j = max_{i<j}...
+    for j1 in range(Params.J):
+        for i1 in range(j1):
+            model.addConstr(z[j1]>=E[i1][j1]*tj[i1]+E[i1][j1]*gp.quicksum([E[i1][j1]*a[j1,k]*a[i1,l]*math.ceil((Params.d_j/Rlk[l][k])/Params.time_window) for l in range(Params.K+1) for k in range(Params.K+1)]))
 
-# Add constraint T_j+t_j^c<=t_j
-model.addConstrs(z[j]+t_jc[j] <= tj[j] for j in range(Params.J))
+    # Compute t_j^c
+    t_jc={}
+    for i1 in range(Params.J):
+        t_jc[i1] = gp.quicksum(a[i1,k]*math.ceil((Params.c_j/Params.f_k)/Params.time_window) for k in range(Params.K+1))
 
-# Add constraint sum_k a_{jk} = 1
-for j in range(Params.J):
-    model.addConstr(gp.quicksum(a[j,k] for k in range(Params.K+1)) == 1)
+    # Add constraint T_j+t_j^c<=t_j
+    model.addConstrs(z[j]+t_jc[j] <= tj[j] for j in range(Params.J))
 
-# a_00=a_J1=1
-model.addConstr(a[0, 0]==1)
-model.addConstr(a[Params.J-1, 0]==1)
-
-# Add constraint ...<n_k
-Compute_slots = math.ceil((Params.c_j/Params.f_k)/Params.time_window)
-
-for k in range(Params.K+1):
-    for ts in range(Params.slots-Compute_slots+1):
-        model.addConstr(gp.quicksum(x[j,k,t] for t in range(ts, ts+Compute_slots-1) for j in range(Params.J))<= Params.nk)
-
-
-
-model.setObjective(tj[Params.J-1], GRB.MINIMIZE)
-
-model.optimize()
-
-print('--------------------------Var Values---------------------------')
-if model.Status==GRB.OPTIMAL:
-    print('# Values of a_jk(Assignment matrix) #')
+    # Add constraint sum_k a_{jk} = 1
     for j in range(Params.J):
+        model.addConstr(gp.quicksum(a[j,k] for k in range(Params.K+1)) == 1)
+
+    # a_00=a_J1=1
+    model.addConstr(a[0, 0]==1)
+    model.addConstr(a[Params.J-1, 0]==1)
+
+    # Add constraint ...<n_k
+    Compute_slots = math.ceil((Params.c_j/Params.f_k)/Params.time_window)
+
+    for k in range(Params.K+1):
+        for ts in range(Params.slots-Compute_slots+1):
+            model.addConstr(gp.quicksum(x[j,k,t] for t in range(ts, ts+Compute_slots-1) for j in range(Params.J))<= Params.nk)
+
+
+
+    model.setObjective(tj[Params.J-1], GRB.MINIMIZE)
+
+    model.optimize()
+
+    print('--------------------------Edge Matrix---------------------------')
+    for i in range(Params.J):
+        print(f'{E[i]}\n')
+    
+
+    print('--------------------------Var Values---------------------------')
+    if model.Status==GRB.OPTIMAL:
+        print('# Values of a_jk(Assignment matrix) #')
+        for j in range(Params.J):
+            ltemp = []
+            for k in range(Params.K+1):
+                ltemp.append(a[j,k].X)
+            print(ltemp)
+        print('# Values of tj(Completion time of tasks) #')
         ltemp = []
-        for k in range(Params.K+1):
-            ltemp.append(a[j,k].X)
+        for _ in range(Params.J):
+            ltemp.append(tj[_].X)
         print(ltemp)
-    print('# Values of tj(Completion time of tasks) #')
-    for _ in range(Params.J):
-        print(tj[_].X)
-    print('# Values of T_j(Ready time of tasks) #')
-    for _ in range(Params.J):
-        print(z[_].X)
-    # print('# n_k in time slot check #')
-    # for j in range(Params.J):
-    #     print(f'----------- Task{j} -----------')
-    #     for k in range(Params.K+1):
-    #         print(f'----------- Task{j} on Server{k} -----------')
-    #         for t in range(Params.slots):
-    #             # print(x[j,k,t].X)
-    #             if x[j,k,t].X == 1:
-    #                 print(f'{t}')
+        ltemp = []
+        print('# Values of T_j(Ready time of tasks) #')
+        for _ in range(Params.J):
+            ltemp.append(z[_].X)
+        print(ltemp)
+        # print('# n_k in time slot check #')
+        # for j in range(Params.J):
+        #     print(f'----------- Task{j} -----------')
+        #     for k in range(Params.K+1):
+        #         print(f'----------- Task{j} on Server{k} -----------')
+        #         for t in range(Params.slots):
+        #             # print(x[j,k,t].X)
+        #             if x[j,k,t].X == 1:
+        #                 print(f'{t}')
+    else:
+        print('Model Optimization Error!')
